a5e76688ffed1e3b98d1e983973399752ae100d6
jcorks <jcorkery@umich.edu>
2015-01-17 13:48:59 -0600

    Added art assets, main game scene

---
+++ Assets/Scripts/Pathway.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* A path between nodes */
+public class Pathway : MonoBehaviour {
+
+	public Vector3 begin;
+	public Vector3 end;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
