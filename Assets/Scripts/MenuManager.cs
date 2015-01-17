e777b7e8dbdaa98f9f0945eb9c58956c79fb3c2f
unknown <rpicking@umich.edu>
2015-01-17 14:39:55 -0500

    Transitions from main menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+    public Menu CurrentMenu;
+
+    public void Start()
+    {
+        ShowMenu(CurrentMenu);
+    }
+
+    public void ShowMenu(Menu menu)
+    {
+        if (CurrentMenu != null)
+            CurrentMenu.isOpen = false;
+        CurrentMenu = menu;
+        CurrentMenu.isOpen = true;
+    }
+}
