50eacffc3f458841904e8c006a1206d805a2cfab
unknown <rpicking@umich.edu>
2015-01-17 13:09:12 -0500

    Main menu & its animations

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    private Animator _animator;
+    private CanvasGroup _canvasgroup;
+
+    public void buttonsound()
+    {
+        audio.Play();
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/MenuAudioAnimation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAudioAnimation : MonoBehaviour {
+
+    public AudioSource stinger;
+
+    void stingersound()
+    {
+        stinger.Play();
+    }
+}
