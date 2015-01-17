f1eabae919a10897fa491774fac153a8844a6f2e
patrshin <patrshin@umich.edu>
2015-01-17 00:14:40 -0800

    Clashes now seem to work as defined

--- Assets/Scripts/troopBehavior.cs
+++ Assets/Scripts/troopBehavior.cs
@@ -1,29 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class troopBehavior : MonoBehaviour {
 
-	public float speed  = 1f;
+	public float speed;
 
-	public float strength = 100;
+	public float strength = 0f;
 
-	public float morale = 200;
+	public float morale = 0f;
 
 	float priorSpeed = 1f;
 
+	float fightInterval = 0;
+
+	public troopBehavior opponent;
+
 	bool fighting = false;
 
+	void makeProperty(float mobilized, float mobilizedMorale) {
+		Debug.Log ("mobilized");
+		strength = mobilized;
+		morale = mobilizedMorale;
+	}
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		makeProperty (100f, 100f);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate() {
+		fightInterval++;
 		//Basic Movement
 		Vector3 pos = transform.position;
 		pos.x += speed * Time.deltaTime;
 		transform.position = pos;
+		if (fightInterval == 50) {
+			if (fighting == true) {
+				Debug.Log("morale:" + morale);
+				Debug.Log("strength:" + strength);
+				strength = Mathf.Round (strength - opponent.strength*0.1f - morale*0.05f);
+				morale = Mathf.Round (morale - opponent.strength*0.15f - morale*0.05f);
+				if (strength < 0) {
+					Destroy (this.gameObject); //Unit destroyed
+				}
+				else if (morale < 0) {
+					speed = priorSpeed*-2; // withdraw if morale is low
+					fighting = false;
+				}
+				if (opponent.morale < 0) {
+					fighting = false;
+					speed = priorSpeed;
+				}
+			}
+
+			fightInterval = 0;
+		}
 		//if (strength == 0)
 			//speed = -2 * priorSpeed;
 			//Destroy (this);
@@ -31,22 +64,16 @@ public class troopBehavior : MonoBehaviour {
 			//speed = -2*priorSpeed;
 	}	
 
-	void OnCollisionEnter(Collision coll){
-			//Find out what hit this basket
+	void OnTriggerEnter(Collider coll){
+		Debug.Log (coll.gameObject);
+		//Find out what hit this basket
 			GameObject collidedWith = coll.gameObject;
 			if (collidedWith.tag == "troop") {
 				fighting = true;
+				opponent = collidedWith.GetComponent<troopBehavior>();
 				Debug.Log ("clash!");
 				priorSpeed = speed;
 				speed = 0;
 			}
-	}
-
-	void OnCollisionStay(Collision coll) {
-		if (fighting && strength != 0) {
-			strength--;
-			Debug.Log (strength);
-			//morale-=3;
-		}
 	}
 }
