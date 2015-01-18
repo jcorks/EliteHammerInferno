3d61037b3e32a3f96f45d6098c8dcb027c33c987
patrshin <patrshin@umich.edu>
2015-01-17 17:08:15 -0800

    heroAttribute added

--- Assets/Scripts/heroAttributes.cs
+++ Assets/Scripts/heroAttributes.cs
@@ -1,15 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
+public enum Hero {
+	Hero_1,
+	Hero_2,
+	Hero_3,
+	Hero_4
+};
